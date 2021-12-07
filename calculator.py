2e3e98d8f28407391b427b3291c2dbaec557b379
seifAfya123 <59228910+seifAfya123@users.noreply.github.com>
2021-12-07 15:56:18 +0200

    application first version

---
+++ calculator.py
@@ -0,0 +1,9 @@
+x= input("enter first number ")
+y= input ("enter second number")
+operation=input("choose opersation")
+if(operation=="+"):
+    result=x+y
+    print("result is : "+result)
+elif(operation=="-"):
+    result=x-y
+    print("result is : "+result )
