6962e07acae6772a687e41a7cbc10c1afaf4dd9e
Omar Mohsen <omar4949mohsen@gmail.com>
2021-12-07 16:28:21 +0200

    while loop

--- calculator.py
+++ calculator.py
@@ -1,9 +1,15 @@
 x= input("enter first number ")
 y= input ("enter second number")
 operation=input("choose opersation")
-if(operation=="+"):
-    result=x+y
-    print("result is : "+result)
-elif(operation=="-"):
-    result=x-y
-    print("result is : "+result )
+again = True
+while(again):
+    if(operation=="+"):
+        again=False
+        result=x+y
+        print("result is : "+result)
+    elif(operation=="-"):
+        again=False
+        result=x-y
+        print("result is : "+result )
+    else:
+        print("undefined operation")
